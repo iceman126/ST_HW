dcbe512520659d704688262d09d05938987cb453
Jin-Bo Huang <iceman1216i@gmail.com>
2017-03-21 02:54:52 +0800

    complete NextDate part of program & test

--- ST_HW1/hw1.h
+++ ST_HW1/hw1.h
@@ -6,11 +6,12 @@
 #include <vector>
 #include <iomanip>
 #include <sstream>
+#include <set>
 
 using namespace std;
 
 string triangle(int a, int b, int c);
 
-// string next_date(int month, int day, int year);
+string next_date(int month, int day, int year);
 
 string commission(int locks, int stocks, int barrels);
--- uint_test/uint_test.cpp
+++ uint_test/uint_test.cpp
@@ -112,6 +112,61 @@ TEST(commission_SR, commission_SR_test) {
 	EXPECT_EQ("locks should be in range of 1~70 & stocks should be in range of 1~80 & barrels should be in range of 1~90", commission(71, 81, 91));
 }
 
+//===============================================================
+
+// Next Date
+
+// Next Date: weak normal
+TEST(next_date_WN, next_date_WN_test) {
+	EXPECT_EQ("6/15/2000", next_date(6, 14, 2000));
+	EXPECT_EQ("7/30/1996", next_date(7, 29, 1996));
+	EXPECT_EQ("Invalid input date", next_date(2, 30, 2002));
+	EXPECT_EQ("Invalid input date", next_date(6, 31, 2000));
+}
+
+// Next Date: strong normal
+TEST(next_date_SN, next_date_SN_test) {
+	EXPECT_EQ("6/15/2000", next_date(6, 14, 2000));
+	EXPECT_EQ("6/15/1996", next_date(6, 14, 1996));
+	EXPECT_EQ("6/15/2002", next_date(6, 14, 2002));
+	EXPECT_EQ("Invalid input date", next_date(6, 31, 1996));
+	EXPECT_EQ("Invalid input date", next_date(6, 31, 2002));
+	EXPECT_EQ("Invalid input date", next_date(2, 30, 2002));
+	EXPECT_EQ("3/1/2002", next_date(2, 28, 2002));
+	EXPECT_EQ("1/1/2003", next_date(12, 31, 2002));
+	EXPECT_EQ("3/1/2000", next_date(2, 29, 2000));
+	EXPECT_EQ("Invalid input date", next_date(2, 29, 1999));
+	EXPECT_EQ("3/1/1999", next_date(2, 28, 1999));
+	EXPECT_EQ("3/1/1999", next_date(2, 28, 1999));
+}
+
+
+// Next Date: weak robust
+TEST(next_date_WR, next_date_WR_test) {
+	EXPECT_EQ("Invalid input date", next_date(0, 31, 1996));
+	EXPECT_EQ("Invalid input date", next_date(13, 31, 1996));
+	EXPECT_EQ("Invalid input date", next_date(6, 32, 2002));
+	EXPECT_EQ("Invalid input date", next_date(6, 0, 2002));
+	EXPECT_EQ("Invalid input date", next_date(6, 31, -1));
+}
+
+// Next Date: strong robust
+TEST(next_date_SR, next_date_SR_test) {
+	EXPECT_EQ("Invalid input date", next_date(0, 31, 1996));
+	EXPECT_EQ("Invalid input date", next_date(13, 31, 1996));
+
+	EXPECT_EQ("Invalid input date", next_date(0, 32, 1996));
+	EXPECT_EQ("Invalid input date", next_date(13, 32, 1996));
+	EXPECT_EQ("Invalid input date", next_date(0, 0, 1996));
+	EXPECT_EQ("Invalid input date", next_date(13, 0, 1996));
+
+	EXPECT_EQ("Invalid input date", next_date(6, 32, 2002));
+	EXPECT_EQ("Invalid input date", next_date(6, 0, 2002));
+	
+	EXPECT_EQ("Invalid input date", next_date(6, 32, -1));
+	EXPECT_EQ("Invalid input date", next_date(6, 0, -1));
+}
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
 
